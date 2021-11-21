98a99831a277dc01eb8cc2e2ef822d406d5afccf
Cobra <93560249+cobramod@users.noreply.github.com>
2021-11-21 02:53:34 +0100

    Link update

--- repo/script.get.build.android/resources/lib/script.py
+++ repo/script.get.build.android/resources/lib/script.py
@@ -76,7 +76,7 @@ def extract(zip_file, output_directory, progress_dialog):
 
 def get_build():
     addon_name = ADDON.getAddonInfo('name')
-    bundleURL = "https://github.com/cobramod/repository.cobramod/blob/main/repo/android/cobra_android.zip"
+    bundleURL = "https://github.com/cobramod/repository.cobramod/raw/main/repo/android/cobra_android.zip"
     bundleVersion = base64.b64decode("aHR0cHM6Ly9naXRodWIuY29tL2NvYnJhbW9kL3JlcG9zaXRvcnkuY29icmFtb2QvYmxvYi9tYWluL3JlcG8vYW5kcm9pZC92ZXJzaW9u").decode("utf-8")
     if not exists(bundleURL):
         xbmcgui.Dialog().ok('Cobra Mod Build [B][COLORred]OFFLINE[/COLOR][/B]','Aktuell leider nicht verfügbar.Versuche es später nochmal...' )
